13db029e137db3cae7a88e5a8ecaee7b1d9efc48
name <email>
2024-05-20 14:59:39 +0700

    Publish

--- hg_translator/model_config.py
+++ hg_translator/model_config.py
@@ -1,8 +1,8 @@
 model_name = "convert_dich"
 num_train_epochs = 30
 
-per_device_train_batch_size = 10
-per_device_eval_batch_size = 10
+per_device_train_batch_size = 8
+per_device_eval_batch_size = 8
 
 checkpoint = "VietAI/vit5-base"
 
