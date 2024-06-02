f7189abd2f3c8f804b961880eabe5323ec2e4e78
name <email>
2024-06-02 18:20:56 +0700

    Publish

--- hg_translator/model_config.py
+++ hg_translator/model_config.py
@@ -2,8 +2,8 @@ model_name = "tien-hiep"
 num_train_epochs = 3
 
 # For GPU P100 16GB
-per_device_train_batch_size = 12
-per_device_eval_batch_size = 12
+per_device_train_batch_size = 8
+per_device_eval_batch_size = 8
 
 # checkpoint = "VietAI/vit5-base"
 checkpoint = "google/mt5-base"
