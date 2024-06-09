ace66e85f63d6ad242cc25fe8c8efc0f4a734f8a
name <email>
2024-06-09 08:23:49 +0700

    Publish

--- train_setup.sh
+++ train_setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-pip install datasets evaluate sacrebleu
-pip install -U accelerate
-pip install -U transformers
+# pip install datasets evaluate sacrebleu
+# pip install -U accelerate
+# pip install -U transformers
+
+
+pip uninstall -y -q tensorflow keras tensorflow-estimator tensorflow-text tf-keras
+pip install protobuf~=3.20.3
+pip install -q "tensorflow-text==2.11.0"
+# pip install -q tensorflow_datasets
+pip install -q "tensorflow==2.11.1" tensorflow_datasets matplotlib
+pip list|grep tensorflow
