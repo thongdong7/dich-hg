94e784b2d8978cc97eb3cde6eda31a55a5c43788
name <email>
2024-06-15 07:04:48 +0700

    Update label

--- tf_translator/train_zh_vi_sample.py
+++ tf_translator/train_zh_vi_sample.py
@@ -5,4 +5,4 @@ from .train_config import sample_config
 
 if __name__ == "__main__":
     os.environ["SAMPLE"] = "1"
-    _train(save_as="zh_vi_transformer_sample", config=sample_config, epochs=1)
+    _train(save_as="zh_vi_transformer_sample", config=sample_config)
